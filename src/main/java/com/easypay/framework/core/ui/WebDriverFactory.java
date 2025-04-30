b24c7faa3bccf48f1d1bcfcc9fbe432a684146aa
Cesar Villarreal <cesarvillarreal11@gmail.com>
2025-04-30 11:31:45 -0400

    Add WebDriverFactory to manage WebDriver instance creation

---
+++ src/main/java/com/easypay/framework/core/ui/WebDriverFactory.java
@@ -0,0 +1,67 @@
+package com.easypay.framework.core.ui;
+
+import com.easypay.framework.config.ConfigManager;
+import io.github.bonigarcia.wdm.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Factory class for creating WebDriver instances.
+ */
+
+public class WebDriverFactory {
+    private static final Logger logger = LoggerFactory.getLogger(WebDriverFactory.class);
+    private static final ConfigManager config = ConfigManager.getInstance();
+
+    /**
+     * Creates a WebDriver instance based on configuration properties.
+     * 
+     * @return WebDriver instance
+     */
+
+    public static WebDriver createDriver() {
+        String browser = config.getProperty("browser", "chrome").toLowerCase();
+        boolean headless = config.getBooleanProperty("headless");
+
+        logger.info("Creating WebDriver for browser: {}, headless: {}", browser, headless);
+
+        WebDriver driver;
+
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions FirefoxOptions = new FirefoxOptions();
+                if (headless) {
+                    FirefoxOptions.addArguements("--headless");
+                }
+                driver = new FirfoxDriver(FirefoxOptions);
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                break;
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (headless) {
+                    chromeOptions.addArguments("--headless");
+                }
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                driver = new ChromeDriver(chromeOptions);
+                break;
+        }
+
+        return driver;
+    }
+
+}
